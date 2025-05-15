4492024fd9b29c4c4ff168240c5eb81f41f7cdc3
fathironmy2 <fathironmy2@gmail.com>
2025-05-15 12:58:33 +0000

    fix: youtube react error

--- src/layouts/shortcodes/Youtube.jsx
+++ src/layouts/shortcodes/Youtube.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
-import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+// import React from "react";
+// import LiteYouTubeEmbed from "react-lite-youtube-embed";
+// import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const Youtube = ({ id, title, ...rest }) => {
-  return (
-    <LiteYouTubeEmbed
-      wrapperClass="yt-lite rounded-md"
-      id={id}
-      title={title}
-      {...rest}
-    />
-  );
-};
+// const Youtube = ({ id, title, ...rest }) => {
+//   return (
+//     <LiteYouTubeEmbed
+//       wrapperClass="yt-lite rounded-md"
+//       id={id}
+//       title={title}
+//       {...rest}
+//     />
+//   );
+// };
 
-export default Youtube;
+// export default Youtube;
